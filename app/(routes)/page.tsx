fcc7360f39e26a2ddb1932524572c7a645a4a2a9
Jorge Matos <jisapjisap@gmail.com>
2023-09-29 21:02:39 +0200

    Product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,7 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container"
 
 export const revalidate = 0;
@@ -7,12 +9,19 @@ export const revalidate = 0;
 const HomePage = async() => {
 
   const billboard = await getBillboard("ffaadcae-2150-43ee-9b66-b9c9ff56bdaf")
+  const products = await getProducts({ isFeatured: true })
 
   return (
     <div>
       <Container>
         <div className="space-y-10 pb-10">
           <Billboard data={billboard}/>
+        </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList 
+            title="Featured Products"
+            items={products}
+          />
         </div>
       </Container>
     </div>
