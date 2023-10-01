8244cc0cf66549cb864cd8328812669bb7a15d3c
Jorge Matos <jisapjisap@gmail.com>
2023-10-01 11:46:27 +0200

    Grid product list

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -13,7 +13,7 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
         { items.length === 0 && <NoResults /> }
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           { items.map((item) => (
-            <div>
+            <div key={item.id}>
               {item.name}
             </div>  
           ))}
