e36f75d16f3d9551ecccb88bf8b92c9327f836c8
Jorge Matos <jisapjisap@gmail.com>
2023-10-01 11:44:40 +0200

    Grid product list

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/types";
+import NoResults from "./ui/no-results";
 
 interface ProductListProps {
     title: string;
@@ -9,6 +10,14 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
   return (
     <div className="sapace-y-4">
         <h3 className="font-bold text-3xl">{title}</h3>
+        { items.length === 0 && <NoResults /> }
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          { items.map((item) => (
+            <div>
+              {item.name}
+            </div>  
+          ))}
+        </div>
     </div>
   )
 }
